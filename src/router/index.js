1b187c82c25038611fb6548abedb77f4380fb2b1
rhqD <eng-123@qq.com>
2019-04-20 17:32:26 +0800

    增加/createCamp路由
    增加createCamp路由，用于增加训练营

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import SignIn from '@/components/SignIn'
 import SignUp from '@/components/SignUp'
+import CreateCamp from '@/components/CreateCamp'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/signup',
       name: 'SignUp',
       component: SignUp
+    },
+    {
+      path: '/createCamp',
+      name: 'CreateCamp',
+      component: CreateCamp
     }
   ]
 })
