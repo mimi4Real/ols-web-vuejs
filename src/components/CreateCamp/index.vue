1227b5ade522384a9f88ae12390b64c8c9ea083a
rhqD <eng-123@qq.com>
2019-04-20 19:41:51 +0800

    创建训练营页面布局完成
    完成创建训练营页面的布局
    完成前端验证和提交流程

--- src/components/CreateCamp/index.vue
+++ src/components/CreateCamp/index.vue
@@ -1,12 +1,23 @@
 <template>
-  <el-form :model="form" :rules="rules" ref="form" label-width="120px">
-    <el-form-item label="标题" prop="userName">
-      <el-input v-model="form.title"></el-input>
+  <el-form :model="form" :rules="rules" ref="form" Id="form">
+    <span Id="returnLink" @click="goBack()">返回</span>
+    <div Id="titleContainer">
+      <span>创建训练营</span>
+    </div>
+    <el-form-item label="标题" class="inputItem" prop="title">
+      <el-col :span="24">
+        <el-input v-model="form.title" Id="titleInput"></el-input>
+      </el-col>
     </el-form-item>
-    <el-form-item label="描述" prop="password">
-      <el-input type="textarea" v-model="form.description"></el-input>
+    <el-form-item label="描述" prop="desc" class="inputItem">
+      <el-col :span="24">
+        <el-input type="textarea" v-model="form.desc" maxLength="300" Id="descInput"></el-input>
+      </el-col>
     </el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
+    <div Id="formFooter">
+      <el-button type="text" @click="goBack()">取消</el-button>
+      <el-button type="primary" @click="submitForm('ruleForm')">保存</el-button>
+    </div>
   </el-form>
 </template>
 
@@ -22,7 +33,12 @@
         },
         rules: {
           title: [
-            {required: true, message: '请输入标题', trigger: 'blur'}
+            {validator: (rule, value, callback) => {
+              if (!value){
+                callback(new Error('请输入标题'));
+              }
+              callback();
+            }, trigger: 'blur'}
           ],
           description: [
             {maxLength: 300, message: '描述不能超过300个字符', trigger: 'blur'}
@@ -32,12 +48,86 @@
     },
     methods: {
       submitForm(formName) {
-        debugger
-        api.postCamp();
+        this.$refs.form.validate((valid) => {
+          if (valid){
+            api.postCamp({...this.form}).then(() => {
+              
+            });
+          }
+        });
+      },
+      goBack() {
+        this.$router.go(-1);
       }
     }
   }
 </script>
 
 <style>
+  .inputItem {
+    display: flex;
+  }
+
+  .inputItem > .el-form-item__content {
+    flex: 1;
+  }
+
+  #form {
+    margin-left: 50px;
+    margin-top: 30px;
+    width: 500px;
+  }
+
+  #returnLinkContainer {
+    width: 120px;
+    text-align: right;
+  }
+
+  #returnLink {
+    width: 36px;
+    height: 25px;
+    font-family: PingFangSC;
+    font-size: 18px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    color: #00b4c5;
+    cursor: pointer;
+  }
+
+  #titleContainer {
+    padding: 25px 0;
+  }
+
+  #titleContainer > span {
+    width: 90px;
+    height: 25px;
+    font-family: PingFangSC;
+    font-size: 18px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    color: #000000;
+  }
+
+  #titleInput {
+    height: 40px;
+    border-radius: 4px;
+    border: solid 1px #979797;
+  }
+
+  #descInput {
+    height: 156px;
+    border-radius: 4px;
+    border: solid 1px #979797;
+    resize: none;
+  }
+
+  #formFooter {
+    text-align: right;
+  }
 </style>
